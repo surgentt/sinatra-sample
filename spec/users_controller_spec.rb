62fd13703892418a68d9024cc9493d5328a9d7d8
Thomas Surgent <surgentt@gmail.com>
2025-08-29 11:50:34 -0400

    Add a controller test

--- spec/users_controller_spec.rb
+++ spec/users_controller_spec.rb
@@ -15,4 +15,18 @@ RSpec.describe "Users Endpoint" do
       expect(data.pluck('id', 'name', 'email')).to eq(users.pluck(:id, :name, :email))
     end
   end
+
+  context 'post /users' do
+    let(:payload) { { name: "Ava", email: "ava@example.com" }.to_json }
+
+    it "returns 201" do
+      post "/users", payload, {
+        "CONTENT_TYPE" => "application/json",
+        "ACCEPT"       => "application/json"
+      }
+      expect(last_response.status).to eq(201)
+      data = JSON.parse(last_response.body)
+      expect(data.slice('name', 'email')).to eq({"name"=>"Ava", "email"=>"ava@example.com"})
+    end
+  end
 end
